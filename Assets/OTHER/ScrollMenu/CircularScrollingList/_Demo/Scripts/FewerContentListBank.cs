8ad6776d99bc7a3b46e6b267c10e45fc3477c1c4
Aleh Yablonski <aleh.yablonski@LeverX.local>
2021-05-26 17:20:06 +0300

    added a new menu

---
+++ Assets/OTHER/ScrollMenu/CircularScrollingList/_Demo/Scripts/FewerContentListBank.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class FewerContentListBank : BaseListBank
+{
+	private string[] contents = {
+		"Settings", "New Game", "Continue",
+	};
+
+	public override string GetListContent(int index)
+	{
+		return contents[index];
+	}
+
+	public override int GetListLength()
+	{
+		return contents.Length;
+	}
+
+}
---
+++ Assets/OTHER/ScrollMenu/CircularScrollingList/_Demo/Scripts/MyApplication.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MyApplication : MonoBehaviour
+{
+    public ListPositionCtrl list;
+    public Text[] ListOfButtons;
+
+    public void Update()
+    {
+        int contentID = list.GetCenteredContentID();
+        string centeredContent = list.listBank.GetListContent(contentID);
+
+        foreach (Text button in ListOfButtons)
+        {
+            if (button.text == centeredContent && button.text != "Continue")
+            {
+                button.CrossFadeAlpha(1, 0, false);
+            } else
+            {
+                if (button.text == "Continue")
+                {
+                    button.CrossFadeAlpha(0.2f, 0, false);
+                } else
+                {
+                    button.CrossFadeAlpha(0.5f, 0, false);
+                }
+            }
+        }
+    }
+}
+
