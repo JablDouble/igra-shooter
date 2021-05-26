52b0cf467b44fd50d5ed6195392614251b136586
Oleg Jablonski <oleg.webster@gmail.com>
2021-05-27 02:39:01 +0300

    added a comics

--- Assets/OTHER/ScrollMenu/CircularScrollingList/_Demo/Scripts/MyApplication.cs
+++ Assets/OTHER/ScrollMenu/CircularScrollingList/_Demo/Scripts/MyApplication.cs
@@ -5,27 +5,35 @@ public class MyApplication : MonoBehaviour
 {
     public ListPositionCtrl list;
     public Text[] ListOfButtons;
+    public Image[] ListOfImages;
 
     public void Update()
     {
         int contentID = list.GetCenteredContentID();
         string centeredContent = list.listBank.GetListContent(contentID);
 
+
+        int i = 0;
         foreach (Text button in ListOfButtons)
         {
             if (button.text == centeredContent && button.text != "Continue")
             {
-                button.CrossFadeAlpha(1, 0, false);
+                button.CrossFadeAlpha(0.9f, 0, false);
+                ListOfImages[i].CrossFadeAlpha(0.9f, 0, false);
             } else
             {
                 if (button.text == "Continue")
                 {
                     button.CrossFadeAlpha(0.2f, 0, false);
+                    ListOfImages[i].CrossFadeAlpha(0.2f, 0, false);
                 } else
                 {
-                    button.CrossFadeAlpha(0.5f, 0, false);
+                    button.CrossFadeAlpha(0.4f, 0, false);
+                    ListOfImages[i].CrossFadeAlpha(0.4f, 0, false);
                 }
             }
+
+            i++;
         }
     }
 }
