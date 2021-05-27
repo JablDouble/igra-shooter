f6241d8c44be99e91bd04930301e7a5bc4fd4d8f
Aleh Yablonski <aleh.yablonski@LeverX.local>
2021-05-27 18:01:07 +0300

    added a fade

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -2,12 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
 
 public class ChangeScene : MonoBehaviour
 {
-    public void setScene(int sceneID)
+
+    public GameObject Canvas;
+    public GameObject MainMusic;
+    public Image blackImage;
+    Animator animFade;
+    Animator musicFade;
+
+    public void Start()
     {
-        SceneManager.LoadScene(sceneID);
+        animFade = Canvas.GetComponent<Animator>();
+        musicFade = MainMusic.GetComponent<Animator>();
     }
 
+    public void setScene(int sceneID)
+    {
+        StartCoroutine(Transition(sceneID));
+    }
+
+    IEnumerator Transition(int sceneID)
+    {
+        blackImage.gameObject.SetActive(true);
+        animFade.SetBool("isFade", true);
+        musicFade.SetBool("isMusicTransition", true);
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(sceneID);
+    }
 }
